5c843b4e97fa61230653cfd07a2740f3f8bf3cf5
MaxiColman <colmanmaximo2003@gmail.com>
2026-08-14 00:34:35 -0300

    Se realizó avances con respecto a la actividad 1

--- tp1/ej1.ts
+++ tp1/ej1.ts
@@ -1,4 +1,38 @@
-const ejemplo1 : string = "Hola Mundo";
-console.log(ejemplo1);
+class Producto {
+    nombre: string;
+    precio: number;
+    categoria: string;
+    stock: number;
 
+    constructor(nombre: string, precio: number, categoria: string, stock: number){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+        this.stock = stock;  
+    }
+    describir(): string {
+        return `${this.nombre} ${this.categoria}: $${this.precio} - ${this.stock} unidades`
+    }
+    hayStock(cantidad: number): boolean {
+        if (cantidad <= this.stock){
+            return true
+        } else {
+            return false
+        }
+    }
 
+    // tome la decisión de poner ERROR porque si es un caso que el 
+    // trabajador obtenga una respuesta paro no lea la pantalla puede 
+    // que vaya a buscar el producto o autorice la venta sin tener el stock, 
+    // cortando de raiz el proceso esto no sucederia
+
+    venderUnidades(cantidad: number): void {
+        if (this.hayStock(cantidad)){
+            this.stock = this.stock - cantidad;
+        } else {
+            throw new Error(`No se puede vender ${this.nombre} ya que solo no suficiente hay en stock`)
+        }        
+    }
+}
+
+const miProducto = new Producto("Chocolate", 1000, "Comidas", 10);
