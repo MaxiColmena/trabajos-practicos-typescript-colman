3f6af0a28cc2c8ea1311a62144ae35a43bbbbbf0
MaxiColman <colmanmaximo2003@gmail.com>
2026-08-25 00:35:17 -0300

    Se realizó el ejercicio 2 y se comenzó el 3

--- tp1/ej3.ts
+++ tp1/ej3.ts
@@ -1,2 +1,24 @@
-const ejemplo3 : string = "Hola Mundo 3";
-console.log(ejemplo3);
+class Empleado {
+    protected nombre: string;
+    protected antiguedad: number
+
+    constructor(nombre: string, antiguedad: number){
+        this.nombre = nombre;
+        this.antiguedad = antiguedad;
+    }
+    calcularSueldo(): number {
+        return 0
+    }
+    describir(): string {
+        return(`${this.nombre} (${this.antiguedad} años) - sueldo: $${this.calcularSueldo()}`)
+    }
+}
+
+ class EmpleadoFijo extends Empleado {
+    private sueldoBase: number;
+
+    constructor(nombre: string, antiguedad: number, sueldoBase: number){
+        super(nombre, antiguedad);
+        this.sueldoBase = sueldoBase;
+    }
+}
