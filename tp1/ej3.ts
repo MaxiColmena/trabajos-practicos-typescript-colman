b064c03d1281c3575b5c2d089467d0a484f777a5
MaxiColman <colmanmaximo2003@gmail.com>
2026-08-25 22:52:33 -0300

    Se realizó la actividad 3

--- tp1/ej3.ts
+++ tp1/ej3.ts
@@ -15,10 +15,22 @@ class Empleado {
 }
 
  class EmpleadoFijo extends Empleado {
+    //Agregamos el atributo que es exclusivo de esta subclase
     private sueldoBase: number;
 
+    // El constructor recibe los datos del padre + el dato nuevo del hijo
     constructor(nombre: string, antiguedad: number, sueldoBase: number){
         super(nombre, antiguedad);
         this.sueldoBase = sueldoBase;
     }
+
+    calcularSueldo(): number {
+        return this.sueldoBase + (this.sueldoBase * 0.02 * this.antiguedad)
+    }
 }
+
+const empleado = new EmpleadoFijo("Maxi", 10, 1000000)
+
+console.log(`tu sueldo es: ${empleado.calcularSueldo()}`)
+
+console.log(empleado.describir());
